f24182d8df877f7b1f5c74f6bd4dd527c7f3294a
Steph-en <appahstephen9@gmail.com>
2021-02-21 18:22:30 +0000

    done with html css and js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function switch_on()
     document.getElementById('img').src = "lightOn.jpeg";
 }
 
-function switch_on()
+function switch_off()
 {
     document.getElementById('img').src = "lightOff.jpg";
 }
